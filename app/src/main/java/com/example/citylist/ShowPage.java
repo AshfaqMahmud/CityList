2e04fbb3fc963c1582dde447f17507b8ed7cd723
Ashfaq Mahmud <ash.mse21@gmail.com>
2022-06-12 00:02:34 +0600

    showpage

---
+++ app/src/main/java/com/example/citylist/ShowPage.java
@@ -0,0 +1,14 @@
+package com.example.citylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_page2);
+    }
+}
