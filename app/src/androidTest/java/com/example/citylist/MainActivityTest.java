e8f0ea36c7be31d6d6219cc348e7af1c4e5dd87d
Ashfaq Mahmud <ash.mse21@gmail.com>
2022-06-12 00:52:48 +0600

    showpage ui test

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -75,5 +75,37 @@ public class MainActivityTest {
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
         Espresso.pressBack(); //Back button
     }
+    @Test
+    public void uitest()
+    {
+        
+
+        
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("California"));
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("UAE"));
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Deutschland"));
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+        onView(withId(R.id.showpage)).check(matches(isDisplayed()));
+        onView(withText("California")).check(matches(isDisplayed()));
+        onView(withId(R.id.button_back)).perform(click());
+        onView(withId(R.id.main)).check(matches(isDisplayed()));
+    }
 
 }
--- app/src/main/java/com/example/citylist/ShowPage.java
+++ app/src/main/java/com/example/citylist/ShowPage.java
@@ -2,7 +2,11 @@ package com.example.citylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShowPage extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class ShowPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_page2);
+        Intent intent =getIntent();
+        String city_name=intent.getStringExtra("cityname");
+        TextView tView=findViewById(R.id.city);
+        tView.setText(city_name);
+
+        Button btn=findViewById(R.id.button_back);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1=new Intent(ShowPage.this,MainActivity.class);
+                startActivity(intent1);
+                finish();
+            }
+        });
     }
 }
